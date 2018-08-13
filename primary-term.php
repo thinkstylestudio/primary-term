03280f47fab49e066a153e6c080a761d69f5060d
Theron Smith <theron@thinkstylestudio.com>
2018-08-13 02:21:50 -0600

    fixed plugin description and license block

--- primary-term.php
+++ primary-term.php
@@ -2,17 +2,28 @@
 
 /*
 Plugin Name: Primary Term
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Plugin URI: http://thinkstylestudio.com
+Description: A WordPress plugin that allows a user to set a primary category.
 Version: 1.0
-Author: t
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Author: Theron Smith
+Author URI: http://thinkstylestudio.com
+License: GPL2
+
+Primary Term is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Primary Term is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Primary Term. If not, see {License URI}.
 */
-opcache_reset();
 
 // If this file is accessed directory, then abort.
-
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
